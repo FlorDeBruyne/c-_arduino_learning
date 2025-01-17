05211a441e145075687fa63ea300da851b06ccf0
FlorDeBruyne <flor.debruyne@gmail.com>
2025-01-17 22:18:03 +0100

    Add variables example in Phase01 with basic data types and output

---
+++ Phase01/README.md
@@ -0,0 +1,7 @@
+Common Data Types:
+int - stores whole numbers
+float - stores decimal numbers (single precision)
+double - stores larger/more precise decimal numbers
+char - stores single character
+bool - stores true or false
+string - stores a sequence of characters (requers <string>)
---
+++ Phase01/variables.cpp
@@ -0,0 +1,22 @@
+// type variable_name = value; //optional value (initializaiton)
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int age = 25;
+    float height = 6.0;
+    char grade = 'A';
+    bool isStudent = false;
+    string name = "Flor";
+
+    // Display variables
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
