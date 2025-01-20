027495e065996199bace6055438b9813f9c56b0d
FlorDeBruyne <flor.debruyne@gmail.com>
2025-01-20 20:21:56 +0100

    Add simple calculator implementation in Phase01 for basic arithmetic operations

---
+++ Phase01/exercises/calculator.cpp
@@ -0,0 +1,27 @@
+//    - Build a simple calculator for addition, subtraction, multiplication, and division.
+#include <iostream>
+
+float addition(float a, float b);
+float substraction(float a, float b);
+float multiplication(float a, float b);
+float division(float a, float b);
+
+
+float addition(float a, float b) {
+    return a +b;
+}
+
+
+float substraction(float a, float b) {
+    return a - b;
+}
+
+
+float multiplication(float a, float b){
+    return a * b;
+}
+
+
+float division(float a, float b){
+    return a / b;
+}
